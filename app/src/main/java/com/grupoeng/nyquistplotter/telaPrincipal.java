eafc598c8a986d7cbe152871e6ac14bd4645560c
Rafael Savenhago <rafael.savenhago@gmail.com>
2018-06-19 11:43:12 -0300

    commit inicial

---
+++ app/src/main/java/com/grupoeng/nyquistplotter/telaPrincipal.java
@@ -0,0 +1,13 @@
+package com.grupoeng.nyquistplotter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class telaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+    }
+}
